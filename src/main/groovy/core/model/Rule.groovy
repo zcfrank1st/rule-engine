ffb69269610364ca65499b4c336e82542820914f
zcfrank1st <zcfrank1st@sina.com>
2017-01-04 18:13:45 +0800

    change build.gradle

--- src/main/groovy/core/model/Rule.groovy
+++ src/main/groovy/core/model/Rule.groovy
@@ -4,5 +4,9 @@ package core.model
  * Created by zcfrank1st on 04/01/2017.
  */
 class Rule {
+    private int version;
 
+    public Rule (int version) {
+        this.version = version
+    }
 }
--- src/main/groovy/implement/DemoStrategy.groovy
+++ src/main/groovy/implement/DemoStrategy.groovy
@@ -1,13 +1,17 @@
 package implement
 
+import com.google.gson.Gson
+import core.model.Rule
 import core.strategy.RuleManipulateStrategy
 
 /**
  * Created by zcfrank1st on 04/01/2017.
  */
 class DemoStrategy implements RuleManipulateStrategy<Object> {
+    private static final Gson gson = new Gson()
+
     @Override
     Object calculate(Object input) {
-        return "hello"
+        return gson.toJson(new Rule(1))
     }
 }
